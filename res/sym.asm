446f45fa653c6e0d396d17f7daf52f025fe67e32
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-10-11 06:29:42 +0200

    update example file

--- res/sym.asm
+++ res/sym.asm
@@ -5,9 +5,8 @@
 
 ; functions
 
-setcglob 0x00000000, app_WinMain
-
 ; imports
 
 setcglob 0x00000000, _imp__LoadLibraryA
+setcglob 0x00000000, _imp__GetModuleHandleA
 setcglob 0x00000000, _imp__GetProcAddress
