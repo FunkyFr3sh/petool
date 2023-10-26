217673c548461df83dc313cdbe8311a901f43de1
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-10-26 13:01:56 +0200

    comment out WinMain template until c++ is ready to be enabled by default

--- res/src/start.c
+++ res/src/start.c
@@ -12,11 +12,12 @@ void start()
     app_start();
 }
 
-// Required for c++ - Hook WinMain here and make sure to add the symbol for the real WinMain to sym.asm
-//CALL(0x00000000, _fake_WinMain);
+7/* Required for c++ - Hook WinMain here and make sure to add the symbol for the real WinMain to sym.asm
+CALL(0x00000000, _fake_WinMain);
 
 int WinMainCRTStartup (void);
 int APIENTRY fake_WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
     return WinMainCRTStartup();
 }
+*/
