79e7cd46b6dcc9cbe352b92dc6023c92b312a443
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-04-07 00:11:30 +0200

    add missing macros to patch.h and sort asm macros

--- res/inc/macros/patch.s
+++ res/inc/macros/patch.s
@@ -15,14 +15,6 @@ _patchset_end_\@:
     .fill (\end) - (\start), 1, (\value)
 .endm
 
-.macro @CLEAR_INT start:req, end:req
-    @CLEAR (\start), 0xCC, (\end)
-.endm
-
-.macro @CLEAR_NOP start:req, end:req
-    @CLEAR (\start), 0x90, (\end)
-.endm
-
 .macro @SJMP src:req, dst:req
     .section .patch,"d0"
     .long (\src)
@@ -68,4 +60,12 @@ _patchset_end_\@:
     .section .text
     .align 8, 0xCC
     dest\addr\():
+.endm
+
+.macro @CLEAR_INT start:req, end:req
+    @CLEAR (\start), 0xCC, (\end)
+.endm
+
+.macro @CLEAR_NOP start:req, end:req
+    @CLEAR (\start), 0x90, (\end)
 .endm
