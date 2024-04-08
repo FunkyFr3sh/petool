0e462a1a12c4beed150a11ede6e82fe8e1d83ed0
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-04-09 01:16:01 +0200

    fix .S file compile error

--- src/genmak.c
+++ src/genmak.c
@@ -89,7 +89,7 @@ int genmak(int argc, char **argv)
 
     fprintf(ofh, "\n");
 
-    fprintf(ofh, "ASFLAGS     = -Iinc -msyntax=intel -mnaked-reg\n");
+    fprintf(ofh, "ASFLAGS     = -Iinc\n");
     fprintf(ofh, "NFLAGS      = -Iinc -f elf\n");
     fprintf(ofh, "CFLAGS      = -Iinc -O2 -march=pentium4 -Wall -masm=intel\n");
     fprintf(ofh, "CXXFLAGS    = -Iinc -O2 -march=pentium4 -Wall -masm=intel\n");
