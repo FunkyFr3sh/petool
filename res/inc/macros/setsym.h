be73a41d02ea525456f28a9ef2a3be128c2a035c
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-07-05 04:55:18 +0200

    #2 improve performance of SETWATGLOB macro

--- res/inc/macros/setsym.h
+++ res/inc/macros/setsym.h
@@ -13,8 +13,58 @@
         ".equ " #name ", " #addr ";"                 \
         ".section .text;"                            \
         ".align 8, 0xCC;"                            \
-        "_" #name ":;"                               \
-        "push " #addr ";"                            \
-        "push " #arg_count ";"                       \
-        "jmp _watcall;"                              \
+        ".ifc " #arg_count ", 0;"                    \
+            "_" #name ":;"                           \
+            "push ebx;"                              \
+            "call " #addr ";"                        \
+            "pop ebx;"                               \
+            "ret;"                                   \
+        ".else;"                                     \
+        ".ifc " #arg_count ", 1;"                    \
+            "_" #name ":;"                           \
+            "push ebx;"                              \
+            "mov eax, dword[esp+4];"                 \
+            "call " #addr ";"                        \
+            "pop ebx;"                               \
+            "ret;"                                   \
+        ".else;"                                     \
+        ".ifc " #arg_count ", 2;"                    \
+            "_" #name ":;"                           \
+            "push ebx;"                              \
+            "mov edx, dword[esp+8];"                 \
+            "mov eax, dword[esp+4];"                 \
+            "call " #addr ";"                        \
+            "pop ebx;"                               \
+            "ret;"                                   \
+        ".else;"                                     \
+        ".ifc " #arg_count ", 3;"                    \
+            "_" #name ":;"                           \
+            "push ebx;"                              \
+            "mov ebx, dword[esp+12];"                \
+            "mov edx, dword[esp+8];"                 \
+            "mov eax, dword[esp+4];"                 \
+            "call " #addr ";"                        \
+            "pop ebx;"                               \
+            "ret;"                                   \
+        ".else;"                                     \
+        ".ifc " #arg_count ", 4;"                    \
+            "_" #name ":;"                           \
+            "push ebx;"                              \
+            "mov ecx, dword[esp+16];"                \
+            "mov ebx, dword[esp+12];"                \
+            "mov edx, dword[esp+8];"                 \
+            "mov eax, dword[esp+4];"                 \
+            "call " #addr ";"                        \
+            "pop ebx;"                               \
+            "ret;"                                   \
+        ".else;"                                     \
+            "_" #name ":;"                           \
+            "push " #addr ";"                        \
+            "push " #arg_count ";"                   \
+            "jmp _watcall;"                          \
+        ".endif;"                                    \
+        ".endif;"                                    \
+        ".endif;"                                    \
+        ".endif;"                                    \
+        ".endif;"                                    \
     )
