486a659f05fc9499bf95b2d90b7b769146ee3b24
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-10-30 20:47:00 +0100

    tweak error messages

--- src/pe.c
+++ src/pe.c
@@ -36,12 +36,12 @@ bool is_supported_pe_image(int8_t* image, uint32_t length)
 
     FAIL_IF(length < sizeof(IMAGE_DOS_HEADER), "File too small.\n");
     FAIL_IF(dos_hdr->e_magic != IMAGE_DOS_SIGNATURE, "File DOS signature invalid.\n");
-    FAIL_IF(dos_hdr->e_lfanew > length - 4 || dos_hdr->e_lfanew < sizeof(IMAGE_DOS_HEADER), "NT headers not found.\n");
+    FAIL_IF(dos_hdr->e_lfanew > length - 4 || dos_hdr->e_lfanew < sizeof(IMAGE_DOS_HEADER), "NT headers not found in file.\n");
     FAIL_IF(nt_hdr->Signature != IMAGE_NT_SIGNATURE, "File NT signature invalid.\n");
-    FAIL_IF(nt_hdr->FileHeader.Machine != IMAGE_FILE_MACHINE_I386, "Machine type is not i386.\n");
+    FAIL_IF(nt_hdr->FileHeader.Machine != IMAGE_FILE_MACHINE_I386, "File machine type is not i386.\n");
 
     bool is_clr = nt_hdr->OptionalHeader.NumberOfRvaAndSizes > 14 && nt_hdr->OptionalHeader.DataDirectory[14].VirtualAddress;
-    FAIL_IF(is_clr, ".NET assembly not supported.\n");
+    FAIL_IF(is_clr, "File type unsupported (.NET assembly).\n");
 
 cleanup:
     return ret == EXIT_SUCCESS;
